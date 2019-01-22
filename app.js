98ebeb97cef433203e79cad8a204d23dc3ec75c4
Shante Austin <shanteaustin@Shantes-MacBook-Air.local>
2019-01-22 18:02:47 -0500

    Added comments to code

--- app.js
+++ app.js
@@ -2,37 +2,37 @@ const holes = document.querySelectorAll('.hole')
 const score = document.querySelector('.score')
 const alphabet = document.querySelectorAll('.alphabet')
 let lastHole;
-let timeUp = false;
+let timeUp = true;
 let userScore = 0;
 let lastLetter;
 
-
+//Get a random letter on each hole. 
 holes.forEach(hole => {
     const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     let randLetter;
     function rand (){
         randLetter = Math.floor(Math.random() * 26) + 1
     }
+    //While last letter is equal to a random letter run rand function
     while (lastLetter === letters[randLetter]) {
         rand()
     }
+    //Add random letter to hole
     setTimeout(function(){
         hole.childNodes[1].classList.add(letters[randLetter])
         lastLetter = letters[randLetter]
-    }, 10)
+    }, 50)
 
 })
-
+//Get random hole
 function randomHole(holes){
     const randomNum = Math.floor(Math.random() * 8) + 1
     const hole = holes[randomNum]
-    return hole;
-   
+    return hole; 
 }
-
-
+//Show letter for random hole 
 function peep() {
-    const time = Math.floor(Math.random() * 1000 ) + 200;
+    const time = Math.floor(Math.random() * 1500 ) + 500;
     const hole = randomHole(holes)
     hole.classList.add('up')
     setTimeout(() => {
@@ -42,17 +42,25 @@ function peep() {
 }
 
 function startGame(){
+    //When you start game time up is set to false
+    timeUp = false;
+    //Call function peep
     peep()
+    //Game lasts 50 seconds, users score is equal to 0 on the start of the game
     setTimeout(() => {
         timeUp = true;
-    }, 5000)
+        userScore = 0;
+    }, 60000)
 }
 
-function scoreIncrease(){
-    userScore++
-    console.log(userScore)
-    score.textContent = userScore
+function scoreIncrease(hole){
+    //if timeUp is false and the letter is up add to userScore on click
+    if(timeUp === false && hole.classList[2] === 'up'){
+        userScore++
+        score.textContent = userScore
+        hole.classList.remove('up')
+    }
 }
 
-
-holes.forEach(hole => hole.addEventListener('click', scoreIncrease));
+//Loop through holes adding on click function and increase score
+holes.forEach(hole => hole.addEventListener('click', () => scoreIncrease(hole) ))
